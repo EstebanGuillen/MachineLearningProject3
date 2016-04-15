b5fee3ab43c139f382f4bcf4f22ccff8965d1751
Esteban Guillen <esteban@Estebans-MacBook-Pro.local>
2016-04-15 00:16:41 -0600

    added extraction of validation data and strong feature selection using random forests

--- classify.py
+++ classify.py
@@ -14,35 +14,138 @@ from sklearn.cross_validation import StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+from matplotlib import pylab
+from sklearn.cross_validation import StratifiedKFold
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn import preprocessing
 
 
-df = pd.read_csv('train.data', header=None)
+pd.set_option('mode.use_inf_as_null', True)
+
+genres = ["blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
 
 
-imr = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imr = imr.fit(df)
-df_imputed = pd.DataFrame(imr.transform(df.values))
+def classify(X,y,pipe):
+
+   
+    #X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20, random_state=1)
 
 
-X = df_imputed.loc[:, 1:].values
-y = df_imputed.loc[:, 0].values
+    skf = StratifiedKFold(y_train, n_folds=10)
 
+    scores = []
+    confusion_matrix = np.zeros((10,10))
+    for train_index, test_index in skf:
+        X_train_fold, X_test_fold = X_train[train_index], X_train[test_index]
+        y_train_fold, y_test_fold = y_train[train_index], y_train[test_index]
+        pipe.fit(X_train_fold, y_train_fold)
+        y_predict = pipe.predict(X_test_fold)
+        scores.append(pipe.score(X_test_fold, y_test_fold))
+    
+        for p,r in zip(y_predict, y_test_fold):
+            confusion_matrix[p,r] = confusion_matrix[p,r] + 1
+        
+    print('CV accuracy scores: %s' % scores)
+    print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores),np.std(scores)))
+    print(confusion_matrix)
+
+
+    '''
+    scores = cross_val_score(estimator=pipe, X = X_train, y=y_train, cv=10, n_jobs=1)
+    print('CV accuracy scores: %s' % scores)
+    print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores),np.std(scores)))
+
+
+    pipe.fit(X_train, y_train)
+    print('Test Accuracy: %.3f' % pipe.score(X_test, y_test))
+    y_predict = pipe.predict(X_test)
+    confusion = confusion_matrix(y_true=y_test, y_pred=y_predict)
+    print(confusion)
+    '''
+    confusion_normalized = confusion_matrix.astype('float') / confusion_matrix.sum(axis=1)[:, np.newaxis]
+    #print(confusion_normalized)
+
+    pylab.clf()
+    pylab.matshow(confusion_normalized, fignum=False, cmap='Blues', vmin=0, vmax=1.0)
+    ax = pylab.axes()
+    ax.set_xticks(range(len(genres)))
+    ax.set_xticklabels(genres)
+    ax.xaxis.set_label_position('top') 
+    ax.xaxis.set_ticks_position("top")
+    ax.set_yticks(range(len(genres)))
+    ax.set_yticklabels(genres)
+    #pylab.title("Confusion Matrix")
+    pylab.colorbar()
+    pylab.grid(False)
+    pylab.xlabel('Predicted class')
+    pylab.ylabel('True class')
+    pylab.grid(False)
+    pylab.show()
+    
+    
+    
+df = pd.read_csv('train-fft-mfcc.data', header=None)
+df = df.dropna(axis=0)
+
+X = df.iloc[:, 1:].values
+
+y = df.iloc[:, 0].values
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20, random_state=1)
 
+#pipe = Pipeline([('scl', StandardScaler()), ('pca',PCA(n_components=10)) , ('clf', LogisticRegression(random_state=1))])
+#pipe = Pipeline([('scl', StandardScaler()), ('clf', LogisticRegression())])
+#pipe = Pipeline([('scl', StandardScaler()),('clf',SVC(kernel='linear', C=40.0, random_state=1))])
+#pipe = Pipeline([('clf', DecisionTreeClassifier(criterion='entropy',max_depth=50))])
+pipe = Pipeline([('clf', RandomForestClassifier(criterion='entropy',n_estimators=1000,n_jobs=-1))])
+#pipe = Pipeline([('scl', StandardScaler()),('clf', KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski'))])
+#pipe = Pipeline([('scl', StandardScaler()),('clf', GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0)  )])
+#pipe = Pipeline([('scl', StandardScaler()),('clf', GaussianNB()  )])
 
-#pipe_lr = Pipeline([('scl', StandardScaler()),('clf', LogisticRegression(random_state=1))])
-#pipe_lr = Pipeline([('scl', StandardScaler()),('clf', SVC(kernel='linear', C=10.0, random_state=1))])
-#pipe_lr = Pipeline([('scl', StandardScaler()),('clf', DecisionTreeClassifier(criterion='entropy',max_depth=40, random_state=0))])
-#pipe_lr = Pipeline([('scl', StandardScaler()),('clf', RandomForestClassifier(criterion='entropy',n_estimators=1000,n_jobs=4, random_state=1))])
-#pipe_lr = Pipeline([('scl', StandardScaler()),('clf', KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski'))])
-#pipe_lr = Pipeline([('scl', StandardScaler()),('clf', GradientBoostingClassifier(n_estimators=100, learning_rate=1.0,max_depth=1, random_state=0)  )])
-pipe_lr = Pipeline([('scl', StandardScaler()),('clf', GaussianNB()  )])
+#classify(X,y,pipe)
 
-#pipe_lr.fit(X_train, y_train)
-#print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+# Build a forest and compute the feature importances
+#X = preprocessing.scale(X)
+forest = ExtraTreesClassifier(n_estimators=250,
+                              random_state=0)
+
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
 
 
-scores = cross_val_score(estimator=pipe_lr, X = X_train, y=y_train, cv=10, n_jobs=1)
-print('CV accuracy scores: %s' % scores)
-print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores),np.std(scores)))
+top_50 = indices[:50]
+
+X_train = X_train[:,top_50]
+print(X_train.shape)
+classify(X_train,y_train,pipe)
+
+X_test = X_test[:,top_50]
+
+pipe.fit(X_train, y_train)
+print('Test Accuracy: %.3f' % pipe.score(X_test, y_test))
+
+'''
+# Print the feature ranking
+print("Feature ranking:")
+
+for f in range(X.shape[1]):
+    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(X.shape[1]), importances[indices],
+       color="r", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), indices)
+plt.xlim([-1, X.shape[1]])
+plt.show()
+'''
+
+
--- extractFeatures.py
+++ extractFeatures.py
@@ -3,12 +3,21 @@ import numpy as np
 import scipy.io.wavfile as wav
 import scipy 
 from scikits.talkbox.features import mfcc
+import librosa
 
-#
+
 dataDirectories = ["blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
 
-data = np.zeros((900,1014))
-labels = np.zeros((900,))
+numberOfSamples = 900
+numberOfFeaturesPlusLabel = 1014
+data = np.zeros((numberOfSamples,numberOfFeaturesPlusLabel))
+
+numberOfValidationSamples = 100
+numberOfValidationFeatures = 1013
+validationData = np.zeros((numberOfValidationSamples,numberOfValidationFeatures))
+
+
+labels = np.zeros((numberOfSamples,))
 count = 0
 for i in range(0,len(dataDirectories)):
     dir = "genres/" + dataDirectories[i]
@@ -17,22 +26,73 @@ for i in range(0,len(dataDirectories)):
         if fileName.endswith(".wav"):
             sample_rate, X = wav.read(dir + "/" + fileName)
             fft_features = abs(scipy.fft(X)[:1000])
+            
             ceps, mspec, spec = mfcc(X)
-            x = np.mean(ceps[int(4135*1/10):int(4135*9/10)], axis=0)
+            numCeps = len(ceps)
+            x = np.mean(ceps[int(numCeps*1/10):int(numCeps*9/10)], axis=0)
+            
+            #y, sr = librosa.load(dir + "/" + fileName)
+            #energyValues = librosa.feature.rmse(y=y)
+            #energy = np.zeros((1,))
+            #energy[0] =  np.mean(energyValues[0,:])
+            
             data[count,0] = i
             data[count,1:] = np.concatenate((fft_features,x)) 
+            #data[count,1:] = x 
+            
             #print data[count,0], data[count,1], data[count,2], data[count,1010]
             count = count + 1
             if count % 10 == 0:
                 print count
+                
 #write to file
+print data
 
-with open("train.data", "w") as f:
-    for r in range (0,900):
+with open("train-fft-mfcc.data", "w") as f:
+    for r in range (0,numberOfSamples):
         line = ''
-        for c in range (0,1014):
+        for c in range (0,numberOfFeaturesPlusLabel):
             line = line + str(data[r,c])
-            if c != 1013:
+            if c != (numberOfFeaturesPlusLabel -1):
+                line = line + ','
+        line = line + '\n'
+        f.write(line)
+        if r % 10 == 0:
+            print r
+            
+            
+validate_dir = 'validation'
+
+count = 0
+for fileName in os.listdir(validate_dir):
+    if fileName.endswith(".wav"):
+        sample_rate, X = wav.read(validate_dir + "/" + fileName)
+        fft_features = abs(scipy.fft(X)[:1000])
+            
+        ceps, mspec, spec = mfcc(X)
+        numCeps = len(ceps)
+        x = np.mean(ceps[int(numCeps*1/10):int(numCeps*9/10)], axis=0)
+            
+        #y, sr = librosa.load(dir + "/" + fileName)
+        #energyValues = librosa.feature.rmse(y=y)
+        #energy = np.zeros((1,))
+        #energy[0] =  np.mean(energyValues[0,:])
+            
+        
+        validationData[count] = np.concatenate((fft_features,x)) 
+        #data[count,1:] = x 
+            
+        #print data[count,0], data[count,1], data[count,2], data[count,1010]
+        count = count + 1
+        if count % 10 == 0:
+            print count
+            
+with open("validation-fft-mfcc.data", "w") as f:
+    for r in range (0,numberOfValidationSamples):
+        line = ''
+        for c in range (0,numberOfValidationFeatures):
+            line = line + str(validationData[r,c])
+            if c != (numberOfValidationFeatures -1):
                 line = line + ','
         line = line + '\n'
         f.write(line)
